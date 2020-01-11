0181b431c3c8d4a204b75e13a8c08b5651dd94a0
Tarun Kumar Dixit <43788843+tarun-sharma03@users.noreply.github.com>
2020-01-11 10:09:49 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from time import sleep
 for i in range(50):
-    driver = webdriver.Chrome('C:\\Users\\my pc\\Downloads\\chromedriver_win32\\chromedriver')
-    driver.get("http://bit.ly/10dayscb")
+    driver = webdriver.Chrome('C:\\Users\\my pc\\Downloads\\chromedriver_win32\\chromedriver') #use the name of browser and location of driver
+    driver.get("http://google.com")    #the link that you want to open 
     sleep(10)
     driver.close()
